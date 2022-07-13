ed565ad3cf7ca738a0a27cf169b2eeeb5c15ce30
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-07-13 01:55:15 -0500

    Complete 'Loading Fonts' section
    This page looks hideous, but it's okay. I'm learning. I think.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource'
       },
+
+      /* Asset Modules: Fonts */
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
